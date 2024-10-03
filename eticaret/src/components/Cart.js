5357f9451d9b621e6f07a2c48382954ec517f582
Sacredice <122034500+Sacredice@users.noreply.github.com>
2024-10-03 16:53:09 +0300

    Ürün adet sayısı ile görüntülenmesi
    1 den fazla bulunan aynı ürünün ayrı listelenmemesi için ürün adet sayısı eklendi

--- eticaret/src/components/Cart.js
+++ eticaret/src/components/Cart.js
@@ -5,6 +5,21 @@ import {Button, ListGroup, ListGroupItem} from 'reactstrap';
 const Cart = ({cartItems, onRemoveFromCart,onClearCart}) => {
   const totalPrice = cartItems.reduce((total, item) => total + item.price,0);
 
+  // Ödev Çözümü
+
+  const stackedCartItems = []
+  cartItems.forEach(item => {
+    const duplicateItem = stackedCartItems.find(obj => obj.id === item.id)
+    if (duplicateItem) {
+      duplicateItem.quantity += 1;
+    } else {
+      item.quantity = 1;
+      stackedCartItems.push(item);
+    }
+  })
+    
+
+
   const handleRemoveFromCart = (item) =>{
     onRemoveFromCart(item);
     alertify.error(`${item.name} sepetten çıkarıldı!`);
@@ -19,9 +34,9 @@ const Cart = ({cartItems, onRemoveFromCart,onClearCart}) => {
     <div>
     <h3>Sepet</h3>
       <ListGroup>
-        {cartItems.map((item) =>(
+        {stackedCartItems.map((item) =>(
           <ListGroupItem key={item.id}>
-            {item.name} - ${item.price}
+            {item.name} - {item.quantity} Adet - ${item.price}
             <Button color='warning'
             onClick={()=> handleRemoveFromCart(item)}
             style={{float: 'right'}}
